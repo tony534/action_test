1c5479acf4454fe020684509bbb9c7dd79fc8ca1
Tony Gao <wei.gao@dxc.com>
2020-11-19 16:24:13 +1100

    update 11

--- test.sh
+++ test.sh
@@ -8,6 +8,7 @@ wget https://developer.salesforce.com/media/salesforce-cli/sfdx-linux-amd64.tar.
 mkdir sfdx
 tar xJf sfdx-linux-amd64.tar.xz -C sfdx --strip-components 1
 ./sfdx/install
+sfdx help force
 
 echo "######"
 echo $FIRST_NAME
