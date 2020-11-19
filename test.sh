dae950b851f7fa08abc4bcbf531dfd46d2ebf9ae
Tony Gao <wei.gao@dxc.com>
2020-11-19 15:51:55 +1100

    test6

--- test.sh
+++ test.sh
@@ -3,3 +3,8 @@ echo "bash test...."
 node --version
 
 ant -version
+
+wget https://developer.salesforce.com/media/salesforce-cli/sfdx-linux-amd64.tar.xz
+mkdir sfdx
+tar xJf sfdx-linux-amd64.tar.xz -C sfdx --strip-components 1
+./sfdx/install
